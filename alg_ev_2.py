ffecb032c608f14aec21074cda7fa3f857a43d4b
Emilliz <liza.karaseva1003@gmail.com>
2021-12-12 11:30:17 +0300

    Расширенный алг

---
+++ alg_ev_2.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+
+def gcdex(a, b):
+    if b == 0:
+        return a, 1, 0
+    else:
+        d, x, y = gcdex(b, a % b)
+        return d, y, x - y * (a // b)
+print (gcdex(a, b))
