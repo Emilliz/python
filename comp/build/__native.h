e0b6a9037ca275219bcd3f9a4d933aa5707c9514
Emilliz <liza.karaseva1003@gmail.com>
2022-03-14 18:20:12 +0300

    I hope I've done all right

---
+++ comp/build/__native.h
@@ -0,0 +1,14 @@
+#ifndef MYPYC_NATIVE_H
+#define MYPYC_NATIVE_H
+#include <Python.h>
+#include <CPy.h>
+#ifndef MYPYC_DECLARED_tuple_T2OO
+#define MYPYC_DECLARED_tuple_T2OO
+typedef struct tuple_T2OO {
+    PyObject *f0;
+    PyObject *f1;
+} tuple_T2OO;
+static tuple_T2OO tuple_undefined_T2OO = { NULL, NULL };
+#endif
+
+#endif
