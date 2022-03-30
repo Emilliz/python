6cff41c6c74d9a113dccdb8ed90adac6f33dbbfc
Emilliz <liza.karaseva1003@gmail.com>
2022-03-30 22:01:13 +0300

    is that right?..

---
+++ kolmog/check.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+
+import pandas
+import matplotlib.pyplot as plt
+from scipy import stats
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+source_file = os.path.join(script_directory , "source_file.csv")
+
+data = pandas.read_csv(str(source_file))
+data["measurement_value"].plt(kind="bar")
+
+data_frame = pandas.DataFrame(data={
+    "value": data["measurement_value"]
+})
+
+print(stats.kstest(data_frame["value"],
+                   "norm",
+                   (data_frame["value"].mean(), data_frame["value"].std()),
+                   N=len(data_frame["value"])
+                   ))
+print('Математик, от которого мы получили данные, был хорошим - значение P велико. Ура!')
